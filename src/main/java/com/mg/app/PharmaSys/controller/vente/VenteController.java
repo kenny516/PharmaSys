a6458b735e7a928eee888052f24b7dbeb1b064d5
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-01-05 13:32:11 +0300

    Update VenteController.java

--- src/main/java/com/mg/app/PharmaSys/controller/vente/VenteController.java
+++ src/main/java/com/mg/app/PharmaSys/controller/vente/VenteController.java
@@ -1,4 +1,5 @@
 package com.mg.app.PharmaSys.controller.vente;
+
 import com.mg.app.PharmaSys.model.medicament.Medicament;
 import com.mg.app.PharmaSys.model.vente.Vente;
 import com.mg.app.PharmaSys.model.vente.VenteDetail;
@@ -17,21 +18,21 @@ import java.util.List;
 @Controller
 @RequestMapping("/vente")
 public class VenteController {
-    private  VenteService venteService;
-    private  VenteDetailService venteDetailService;
-    private  MedicamentService medicamentService;
-    private  StockService stockService;
+    private VenteService venteService;
+    private VenteDetailService venteDetailService;
+    private MedicamentService medicamentService;
+    private StockService stockService;
 
     @GetMapping
     public String listVente(Model model) {
         List<Vente> ventes = venteService.readVente();
-        model.addAttribute("ventes",ventes);
+        model.addAttribute("ventes", ventes);
         return "vente/VenteListe";
     }
 
     @GetMapping("/edit")
-    public String editVente(@RequestParam(value = "id",required = false) Integer id, Model model) {
-        Vente vente= new Vente();
+    public String editVente(@RequestParam(value = "id", required = false) Integer id, Model model) {
+        Vente vente = new Vente();
         if (id == null) {
             model.addAttribute("vente", vente);
             return "vente/VenteForm";
@@ -56,22 +57,23 @@ public class VenteController {
         venteService.deleteVente(id);
         return "redirect:/vente";
     }
+
     ///  vente details
 
     @GetMapping("/{idVente}/detail")
     public String listVenteDetail(@PathVariable Integer idVente, Model model) {
         Vente vente = venteService.getVenteById(idVente);
-        List< VenteDetail> venteDetails = venteDetailService.getVenteDetailByVente(idVente);
-        model.addAttribute("vente",vente);
-        model.addAttribute("venteDetails",venteDetails);
+        List<VenteDetail> venteDetails = venteDetailService.getVenteDetailByVente(idVente);
+        model.addAttribute("vente", vente);
+        model.addAttribute("venteDetails", venteDetails);
         return "vente/VenteDetailListe";
     }
 
     @GetMapping("/detail/edit")
-    public String editVenteDetail(@RequestParam(value = "idVenteDetail",required = false) Integer idVenteDetail,@RequestParam(value = "idVente",required = false) Integer idVente, Model model) {
+    public String editVenteDetail(@RequestParam(value = "idVenteDetail", required = false) Integer idVenteDetail, @RequestParam(value = "idVente", required = false) Integer idVente, Model model) {
         List<Medicament> medicaments = medicamentService.readMedicaments();
-        model.addAttribute("medicaments",medicaments);
-        VenteDetail venteDetail= new VenteDetail();
+        model.addAttribute("medicaments", medicaments);
+        VenteDetail venteDetail = new VenteDetail();
         Vente vente = venteService.getVenteById(idVente);
         venteDetail.setVente(vente);
         if (idVenteDetail == null) {
@@ -82,54 +84,39 @@ public class VenteController {
         model.addAttribute("venteDetail", venteDetail);
         return "vente/VenteDetailForm";
     }
+
     @PostMapping("/detail/save")
-    public String saveVenteDetail(VenteDetail venteDetail,Model model) {
+    public String saveVenteDetail(VenteDetail venteDetail, Model model) {
         Double quantiteInitial = 0.0;
         if (venteDetail.getId() == null) {
             Medicament medicament = medicamentService.getMedicamentById(venteDetail.getMedicament().getId());
             venteDetail.setPrixUnitaire(medicament.getPrix());
-        }else {
+        } else {
             VenteDetail venteDetailOld = venteDetailService.getVenteDetailById(venteDetail.getId());
             quantiteInitial = venteDetailOld.getQuantite();
-            if (quantiteInitial<venteDetail.getQuantite()){
-                venteDetail.setQuantite(venteDetail.getQuantite()-quantiteInitial);
-            }else {
-                venteDetail.setQuantite(quantiteInitial-venteDetail.getQuantite());
-            }
+            venteDetail.setQuantite(venteDetail.getQuantite() - quantiteInitial);
         }
         try {
             stockService.updateStockForSale(venteDetail);
-            venteDetail.setQuantite(quantiteInitial+venteDetail.getQuantite());
+            venteDetail.setQuantite(quantiteInitial + venteDetail.getQuantite());
         } catch (IllegalArgumentException e) {
             model.addAttribute("errorMessage", e.getMessage());
             return "ErrorPage/Error";
         }
         venteDetailService.createVenteDetail(venteDetail);
         venteService.updateVenteData(venteDetail.getVente().getId());
-        return "redirect:/vente/"+venteDetail.getVente().getId()+"/detail";
+        return "redirect:/vente/" + venteDetail.getVente().getId() + "/detail";
     }
 
     @GetMapping("/detail/delete")
     public String deleteVenteDetail(@RequestParam(value = "idVenteDetail") Integer id) {
         VenteDetail venteDetail = venteDetailService.getVenteDetailById(id);
-        venteDetail.setQuantite(venteDetail.getQuantite()*-1);
+        venteDetail.setQuantite(venteDetail.getQuantite() * -1);
         venteDetailService.deleteVenteDetail(id);
         stockService.updateStockForSale(venteDetail);
         venteService.updateVenteData(venteDetail.getVente().getId());
-        return "redirect:/vente/"+venteDetail.getVente().getId()+"/detail";
+        return "redirect:/vente/" + venteDetail.getVente().getId() + "/detail";
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
