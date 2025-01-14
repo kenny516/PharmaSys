4669dc64f1e16b6a5439dc5fc64afb8f816a5081
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-01-14 15:53:45 +0300

    done fonctionalite 3

--- database/requette.sql
+++ database/requette.sql
@@ -1,8 +1,16 @@
-SELECT distinct v.*
-FROM vente_detail v
-         JOIN Produit p ON v.id_produit = p.id
-         JOIN administration ad ON p.id_administration = ad.id
-         JOIN Produit_public_cible ppc ON p.id = ppc.id_produit
-WHERE p.id_categorie = 1
-  AND p.id_administration = 1
-  AND ppc.id_public = 1;
+-- Ajouter de nouveaux produits
+INSERT INTO Produit (nom, description, prix, perissable,id_administration, id_unite, id_categorie, id_laboratoire)
+VALUES
+    ('Ibuprofène', 'Anti-inflammatoire non stéroïdien.', 5.0, FALSE, 1,1, 2, 1),
+    ('Azithromycine', 'Antibiotique à large spectre.', 12.0, TRUE, 1,2, 1, 2),
+    ('Vitamine C', 'Complément alimentaire pour renforcer le système immunitaire.', 3.0, FALSE, 1,1, 2, 1),
+    ('Oméprazole', 'Inhibiteur de la pompe à protons pour traiter les ulcères.', 7.5, FALSE, 1,1, 1, 2);
+
+
+-- Ajouter des conseils pour les nouveaux produits
+INSERT INTO Produit_conseil (id_produit, date_debut, date_fin, description)
+VALUES
+    (1, '2025-03-01', '2025-03-31', 'Recommandé pour les douleurs articulaires durant le printemps.'),
+    (4, '2025-04-01', '2025-04-30', 'Idéal pour prévenir les infections respiratoires en avril.'),
+    (5, '2025-05-01', '2025-05-31', 'Recommandé pour renforcer l’immunité avant l’été.'),
+    (6, '2025-06-01', '2025-06-30', 'Utilisé pour soulager les symptômes de reflux gastrique en été.');
---
+++ src/main/java/com/mg/app/PharmaSys/controller/produit/ProduitConseilController.java
@@ -0,0 +1,75 @@
+package com.mg.app.PharmaSys.controller.produit;
+
+import com.mg.app.PharmaSys.model.produit.Produit;
+import com.mg.app.PharmaSys.model.produit.ProduitConseil;
+import com.mg.app.PharmaSys.service.produit.ProduitConseilService;
+import com.mg.app.PharmaSys.service.produit.ProduitService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/produi-conseils")
+public class ProduitConseilController {
+
+    private final ProduitConseilService produitConseilService;
+    private final ProduitService produitService;
+
+    @GetMapping
+    public String getAllProduitConseils(Model model) {
+        List<ProduitConseil> produitConseils = produitConseilService.getAllProduitConseils();
+        model.addAttribute("date",LocalDate.now());
+        model.addAttribute("produitConseils", produitConseils);
+        return "produit/produitConseil/produitConseilListe";
+    }
+
+    @PostMapping
+    public ResponseEntity<ProduitConseil> createProduitConseil(@RequestBody ProduitConseil produitConseil) {
+        return ResponseEntity.ok(produitConseilService.saveProduitConseil(produitConseil));
+    }
+
+    @GetMapping("/edit")
+    public String formInsert(@RequestParam(value = "id",required = false) Integer id,Model model) {
+        ProduitConseil produitConseils = new ProduitConseil();
+        if (id!=null){
+            produitConseils = produitConseilService.getProduitConseilById(id);
+        }
+        List<Produit> produits = produitService.readProduits();
+        model.addAttribute("produits",produits);
+        model.addAttribute("produitConseils",produitConseils);
+        return "produit/produitConseil/produitConseilForm";
+    }
+
+    @PostMapping("recherche")
+    public String recherche(@RequestParam(value = "date",required = false) LocalDate date, Model model){
+        if (date == null){
+            date = LocalDate.now();
+        }
+        model.addAttribute("date",date);
+
+        List<ProduitConseil> produitConseils = produitConseilService.rechercheF(date);
+        model.addAttribute("produitConseils", produitConseils);
+        return "produit/produitConseil/produitConseilListe";
+    }
+
+    @PostMapping("/save")
+    public String saveConseil(ProduitConseil produitConseil){
+        produitConseilService.saveProduitConseil(produitConseil);
+        return "redirect:/produi-conseils";
+    }
+
+    @GetMapping("delete")
+    public String delete(@RequestParam("id") Integer id) {
+        produitConseilService.deleteProduitConseilById(id);
+        return "redirect:/produi-conseils";
+    }
+
+}
