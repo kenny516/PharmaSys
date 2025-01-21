9346a173f796953df4e68e407f2386ef00972339
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-01-21 18:50:24 +0300

    add mcd update

--- src/main/java/com/mg/app/PharmaSys/service/vente/VenteService.java
+++ src/main/java/com/mg/app/PharmaSys/service/vente/VenteService.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @AllArgsConstructor
